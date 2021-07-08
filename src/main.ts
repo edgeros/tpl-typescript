39f86c6eb60e27a99368952b84d61ac328d82169
xieyuanbin <xieyuanbin@acoinfo.com>
2021-07-08 10:40:08 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import WebApp from "webapp"
+import myRouter from "./routers/rest"
+import path from 'path'
+
+/* Create App */
+const app = WebApp.createApp();
+
+/* Set static path */
+app.use(WebApp.static(path.resolve(path.dirname(module.directory), '/public')))
+
+/* Set test rest */
+app.use('/api', myRouter)
+
+/* Rend test */
+app.get('/temp.html', function(req, res) {
+	res.render('temp', { time: Date.now() })
+});
+
+/* Start App */
+app.start()
+
+/* Event loop */
+require('iosched').forever()
