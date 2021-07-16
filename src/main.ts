bf080dfea33328b223a291d862bd9df4de29b240
xieyuanbin <xieyuanbin@acoinfo.com>
2021-07-16 21:22:20 +0800

    update tsconfig

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import WebApp from "webapp"
-import myRouter from "./routers/rest"
+import WebApp from 'webapp'
+import myRouter from './routers/rest'
 import path from 'path'
 
 /* Create App */
-const app = WebApp.createApp();
+const app = WebApp.createApp()
 
 /* Set static path */
 app.use(WebApp.static(path.resolve(path.dirname(module.directory), '/public')))
@@ -12,9 +12,9 @@ app.use(WebApp.static(path.resolve(path.dirname(module.directory), '/public')))
 app.use('/api', myRouter)
 
 /* Rend test */
-app.get('/temp.html', function(req, res) {
-	res.render('temp', { time: Date.now() })
-});
+app.get('/temp.html', function (req, res) {
+  res.render('temp', { time: Date.now() })
+})
 
 /* Start App */
 app.start()
